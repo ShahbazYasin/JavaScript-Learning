0b16b797de8aa38265d2bb6977c43bade52caf48
shahbaz yasin <shahbazyasin26@gmail.com>
2023-12-30 11:05:31 -0800

    Javascript

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-document.write("<u>external javascript</u><hr>")
+// document.write("<u>external javascript</u><hr>")
+
+// var A = "This is a Variable"; 
+// A = "This is a reasigning variable"
+// A = "This is a reasigning variable"
+// A = "This is a reasigning variable"
+// A = "This is a reasigning variable"
+
+// var B = 200
+
+// // console.log(A);
+// document.write(A);
+// document.write(B);
+
+var firstName, lastName, fatherName, designation;
+
+firstName = 'Shahbaz';
+lastName = 'Yasin';
+fatherName = 'Ghulam Yasin';
+designation = 'Assistant Accounts Officer';
+
+console.log(firstName, lastName, fatherName, designation);
+document.write(firstName, lastName);
