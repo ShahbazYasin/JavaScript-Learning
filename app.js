ec0277013641f58548403a4c57bbef21850cbf3b
shahbaz yasin <shahbazyasin26@gmail.com>
2023-12-30 21:22:06 -0800

    Javascript

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-// document.write("<u>external javascript</u><hr>")
+document.write("<ol><li>JavaScript Introduction</li>")
+document.write("<li>JavaScript Implementation</li>")
+document.write("<li>HTML Tag in JS</li>")
+document.write("<li>JavaScript Comments</li>")
+document.write("<li>JavaScript Varibles (var)</li>")
+
 
 // var A = "This is a Variable"; 
 // A = "This is a reasigning variable"
@@ -12,12 +17,142 @@
 // document.write(A);
 // document.write(B);
 
-var firstName, lastName, fatherName, designation;
+// var firstName, lastName, fatherName, designation;
 
-firstName = 'Shahbaz';
-lastName = 'Yasin';
-fatherName = 'Ghulam Yasin';
-designation = 'Assistant Accounts Officer';
+// firstName = 'Shahbaz';
+// lastName = 'Yasin';
+// fatherName = 'Ghulam Yasin';
+// designation = 'Assistant Accounts Officer';
 
-console.log(firstName, lastName, fatherName, designation);
-document.write(firstName, lastName);
+// console.log(firstName, lastName, fatherName, designation);
+// document.write(firstName, lastName);
+
+document.write("<li>var, let, const difference</li>");
+
+//var
+
+// var x = "Hellow";
+// var x = "Hey!"
+// x = "How are you?"
+
+// console.log(x);
+
+//let
+
+let y = "Hellow";
+//let y = "Hey!";   //wrong 
+y = "How are you?";
+
+// console.log(y);
+
+//const
+
+const z = "Hellow";
+// const z = "Hey!"; //wrong
+// z = "How are you?"; //wrong
+
+// console.log(z);
+
+document.write("<li>Data Type</li>");
+
+//var x = 'How are you?'; // type string
+
+//var x = 112; // type number
+
+//var x = false; // type boolean
+
+//var x = ["HTML", "CSS", "JS"]; // type Array
+
+/*var x = {                       // type object
+    firstName: "shahbaz",
+    lastName: "yasin",
+}*/
+
+//var x = null;      // type null
+
+//var x = undefined           // type undefined
+
+// console.log(x);
+// console.log(typeof x);
+
+document.write("<li>Arithmetic Operators</li>")
+
+
+// var a = 10;
+// var b = 7;
+// var c = (a + b) +2;
+// console.log(c);
+
+// a++;
+
+// console.log(a+b);
+
+
+document.write("<li>Assignment Operators</li>")
+
+// operator        example     same as
+// =               x = y       x = y
+// +=              x += y      x = x +y
+// -=              x -= y      x = x - y
+// *=              x *= y      x = x* y
+// /=              x /= y      x = x / y
+// %=              x %= y      x = x % y  
+// **=             x **= y     x = x **y
+
+// var a = 2;
+// var b = 3;
+// a **= b;
+
+// console.log(a);
+
+
+
+document.write("<li>Google Chrome Console</li>")
+
+document.write("<ul><li>console.log();</li>")
+document.write("<li>console.table();</li>")
+document.write("<li>console.error();</li>")
+document.write("<li>console.warn();</li>")
+document.write("<li>console.clear();</li>")
+document.write("<li>console.time();</li>")
+document.write("<li>console.timeEnd();</li></ul>")
+
+// var myName = "Shahbaz Yasin";
+
+// console.table(["shahbazy","Yasin", 27]);
+
+// var day = document.getElementById('day').innerHTML
+// console.log(day);
+console.clear();
+
+
+document.write("<li>Comperision Operators</li>") //topic 11
+
+// Different type of comperision operators
+
+// operators           Description
+// ==                  equal to 
+// ===                 equal value and equal type 
+// !=                  not equal
+// !==                 not equal value or not equal type 
+// >                   greater than
+// <                   less than
+// >=                  greater then and equal to
+// <=                  less than and equal to
+
+// a = 22;
+// b = 22;
+
+// console.log(a <= b);
+
+// document.write("<li>If Statement</li>") //topic 12
+
+// var a = 200;
+// var b = 30;
+
+// if(a !== b){
+//     console.log("A is greater than B");
+// }else
+// {
+//     console.log("Not equal");
+// }
